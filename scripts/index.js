c2ccf193d73295f4f16e73e84c797005a3e01e13
Steve Larsen <slarsen-3@att.net>
2021-05-21 19:07:34 -0500

    update firestore users collection and firestore rules

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,60 @@
-// set up the guides
+// DOM elements
 const guideList = document.querySelector(".guides");
-const loggedOutlinks = document.querySelectorAll(".logged-out");
+const loggedOutLinks = document.querySelectorAll(".logged-out");
 const loggedInLinks = document.querySelectorAll(".logged-in");
+const accountDetails = document.querySelector(".account-details");
 
 const setupUI = (user) => {
   if (user) {
-    // toggle ui elements
+    // account info
+    db.collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        const html = `
+      <div>Logged in as ${user.email}</div>
+      <div>${doc.data().bio}</div>
+    `;
+        accountDetails.innerHTML = html;
+      });
+
+    // toggle user UI elements
     loggedInLinks.forEach((item) => (item.style.display = "block"));
-    loggedOutlinks.forEach((item) => (item.style.display = "none"));
+    loggedOutLinks.forEach((item) => (item.style.display = "none"));
   } else {
-    // toggle ui elements
+    // clear account info
+    accountDetails.innerHTML = "";
+    // toggle user elements
     loggedInLinks.forEach((item) => (item.style.display = "none"));
-    loggedOutlinks.forEach((item) => (item.style.display = "block"));
+    loggedOutLinks.forEach((item) => (item.style.display = "block"));
   }
 };
 
+// setup guides
 const setupGuides = (data) => {
   if (data.length) {
     let html = "";
     data.forEach((doc) => {
       const guide = doc.data();
       const li = `
-    <li>
-    <div class="collapsible-header grey lighten-4">${guide.title}</div>
-    <div class="collapsible-body white">${guide.content}</div>
-    </li>
-    `;
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
+          <div class="collapsible-body white"> ${guide.content} </div>
+        </li>
+      `;
       html += li;
     });
-
     guideList.innerHTML = html;
   } else {
-    guideList.innerHTML = "<h5>Login to view guides</h5>";
+    guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
   }
 };
 
 // setup materialize components
-document.addEventListener("DOMContentLoaded", () => {
-  let modals = document.querySelectorAll(".modal");
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
   M.Modal.init(modals);
 
-  let items = document.querySelectorAll(".collapsible");
+  var items = document.querySelectorAll(".collapsible");
   M.Collapsible.init(items);
 });
